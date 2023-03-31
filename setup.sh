eed755bc5f7dac274ceab83575d91eb1560ee94e
drcatherinepope <cpope@gitlab.com>
2023-03-31 10:28:01 +0100

    Initial commit

--- setup.sh
+++ setup.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-find . -type f -exec sed -i 's/{dockerHubUsername}/'$1'/g' {} +
+
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Please provide a value for {dockerHubUsername}"
+  exit 1
+fi
+
+find . -name '*.yaml' -type f -exec sed -i '' -E "s/{dockerHubUsername}/$1/g" {} +
